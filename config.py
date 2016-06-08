18ae40ef1f22331bf4b05f2ccd92247886edc884
Jesus Tinoco <jesus.rodriguez.tinoco@gmail.com>
2016-06-08 22:27:58 +0200

    Add a project architecture

---
+++ config.py
@@ -0,0 +1,11 @@
+DEBUG = True
+
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+THREADS_PER_PAGE = 2
+
+CSRF_ENABLED = True
+CSRF_SSESION_KEY = "secret"
+
+SECRET_KEY = "secret"
