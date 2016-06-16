18296ce8cda2e3bf6d713b2ec32d514af7d4d578
Jesus Tinoco <jesus.rodriguez.tinoco@gmail.com>
2016-06-16 22:07:38 +0200

    First approach to container info

--- app/containers/controllers.py
+++ app/containers/controllers.py
@@ -9,3 +9,9 @@ containers = Blueprint('containers', __name__, url_prefix='/containers')
 def list():
     containerList = cli.containers(all=True)
     return render_template('containers/list.html', containers=containerList)
+
+@containers.route('/<string:container_id>')
+def container_info(container_id):
+    print(container_id)
+    container = cli.inspect_container(container_id)
+    return render_template('containers/info.html', container = container)
