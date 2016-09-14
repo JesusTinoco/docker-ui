9abacaeaef8e71220a3e47e012af144534e52541
Jesus Tinoco <jesus.rodriguez.tinoco@gmail.com>
2016-09-14 23:00:36 +0200

    Added remove network action and improve network search

--- app/networks/controllers.py
+++ app/networks/controllers.py
@@ -12,12 +12,24 @@ def list():
     networks = [] if networks_aux is None else [Network(network) for network in networks_aux]
     return networks_list('networks/list.html', networks)
 
+
+@networks.route('/<string:network_id>/delete', methods=['POST'])
+def remove_network(network_id):
+    try:
+        cli.remove_network(network_id)
+        flash('Network "%s" has been deleted successfully.' % network_id, 'success')
+    except Exception as err:
+        print err
+        flash(str(err), 'warning')
+    return redirect(url_for('networks.list'))
+
 def networks_list(template, network_list):
     search = request.args.get('q')
     networks_filtered = network_list
     if search:
         networks_filtered = []
         for network in network_list:
-            if search in network.name():
+            if (search in network.name()
+                or search in network.id()):
                 networks_filtered.append(network)
     return render_template(template, networks=networks_filtered)
